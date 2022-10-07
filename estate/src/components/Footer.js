54f3ab4460f49a4d2b4809e5635092fa348f19e7
MustafaAzad <mustafaazad533@gmail.com>
2022-10-07 07:35:49 +0530

    Footer Changed

--- estate/src/components/Footer.js
+++ estate/src/components/Footer.js
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-
-
-import React from "react";
-import './footer.css'
-
-function Footer() {
-  return (
-   <>
-	<footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>COMPANY</h4>
-  	 			<ul>
-  	 				<li><a href="#">about us</a></li>
-  	 				<li><a href="#">our services</a></li>
-  	 				<li><a href="#">privacy policy</a></li>
-  	 				<li><a href="#">affiliate program</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>GET HELP</h4>
-  	 			<ul>
-  	 				<li><a href="#">FAQ</a></li>
-  	 				<li><a href="#">shipping</a></li>
-  	 				<li><a href="#">returns</a></li>
-  	 				<li><a href="#">order status</a></li>
-  	 				<li><a href="#">payment options</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>BOOK ONLINE</h4>
-  	 			<ul>
-  	 				<li><a href="#">plots</a></li>
-  	 				<li><a href="#">houses</a></li>
-  	 				<li><a href="#">villa</a></li>
-  	 				<li><a href="#">bunglow</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>FOLLOW US</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i className="fa-twitter"></i></a>
-  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i className="fab fa-instagram"></i></a>
-  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
-  	 			</div>
-  	 		</div>
-			
-  	 	</div>
-		<hr />
-		<div className="row" >
-          		<p className="col-sm">
-            	&copy;{new Date().getFullYear()} Real State App | All rights reserved |Terms Of Service | Privacy
-				</p>
-        	</div>
-  	 </div>
-	 
-  </footer>
-
-
-   </>
-  );
-}
-
-export default Footer;
-=======
 import React from 'react'
 const Footer = () => {
     const social = [
@@ -90,4 +22,3 @@ const Footer = () => {
 }
 
 export default Footer
->>>>>>> 66af8111c1f90a09027ec3b4c803467fe2df6c82
